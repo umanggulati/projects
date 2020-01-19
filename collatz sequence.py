14743550dc277f8795e694072527ab9209e59eca
umanggulati <41832270+umanggulati@users.noreply.github.com>
2020-01-19 17:28:21 +0530

    Add files via upload

---
+++ collatz sequence.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def collatz(number):
+
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+n = input("Give me a number: ")
+while n != 1:
+    n = collatz(int(n))
